4e6b3333792472658d069d23f933f65aa7d2ce4f
Jon <jrobertsonimap@gmail.com>
2021-02-02 16:51:46 -0500

    update

--- resume/urls.py
+++ resume/urls.py
@@ -5,7 +5,7 @@ from resume import views
 
 urlpatterns = [
     path('', views.view_resume, name='v_resume'),
-    path('aboutme/', views.view_resume, name='v_aboutme'),
+    path('aboutme/', views.view_aboutme, name='v_aboutme'),
 ]
 
 if settings.DEBUG:
